58064503e13e9f63e5a33c8e1928a2bb3dd0913b
adoidadox2 <augusto.geek@gmail.com>
2020-06-17 17:11:00 -0300

    add: migrations de users e de posts

---
+++ src/database/migrations/1592424342257-createPost.ts
@@ -0,0 +1,51 @@
+import { MigrationInterface, Table, QueryRunner } from "typeorm";
+
+export default class CreatePost1592424342257 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "post",
+        columns: [
+          {
+            name: "id",
+            type: "uuid",
+            isPrimary: true,
+            generationStrategy: "uuid",
+            default: "uuid_generate_v4()",
+            isNullable: false,
+          },
+          {
+            name: "title",
+            type: "varchar",
+            isUnique: true,
+            isNullable: false,
+          },
+          {
+            name: "content",
+            type: "varchar",
+            isNullable: false,
+          },
+          {
+            name: "created_At",
+            type: "timestamp",
+            default: "now()",
+          },
+          {
+            name: "updated_At",
+            type: "timestamp",
+            default: "now()",
+          },
+          {
+            name: "deleted_At",
+            type: "timestamp",
+            isNullable: true,
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("user");
+  }
+}
