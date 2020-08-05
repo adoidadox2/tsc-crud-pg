5dd589aa99ecdc13e65918041bbe29a98dafaf84
adoidadox2 <augusto.geek@gmail.com>
2020-08-05 15:24:41 -0300

    minor changes

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import app from "./App";
 
-app.listen(process.env.PORT || 3333, () => {
-  console.log("Server On");
+const port: string = process.env.PORT || "3333";
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
 });
